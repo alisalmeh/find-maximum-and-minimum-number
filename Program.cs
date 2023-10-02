5c64fab2ce0319afa422e3ae2ce79326fb790f51
Ali Salmeh <alisalmeh18@gmail.com>
2023-10-02 14:48:20 +0330

    change project name

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace AliSalmeh_ProjectWeek5.FindMaxMin_Refactor
+namespace AliSalmeh_ProjectWeek5_FindMaxMin
 {
     class Program
     {
